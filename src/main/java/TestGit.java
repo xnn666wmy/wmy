d5289c381944f06dff186db654696898450d053b
wmy <wmy@zpark.com>
2018-07-29 23:23:30 +0800

    修改git1

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -1,6 +1,7 @@
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello,我是第一个上传到github的项目！！");
+        System.out.println("hello2,我是第一二个上传到github的项目！！");
     }
 
 
